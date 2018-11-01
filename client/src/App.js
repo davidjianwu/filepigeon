e57e37e0b8e406ce7c461d7c4be6a2b699cbae3f
David <zjdavidwu@gmail.com>
2018-11-01 03:28:11 -0400

    Implemented download feature from s3 cloud

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import './App.css';
 import { Input, Button, List, Upload, Icon } from 'antd';
+import './App.css';
 import 'antd/dist/antd.css'
+const download = require("downloadjs");
 
 class App extends Component {
   constructor(props) {
@@ -9,13 +10,8 @@ class App extends Component {
 
     this.state = {
       key: "",
-      files: [
-        {title: "Tom.txt"},
-        {title: "AnImportantDoc.txt"},
-        {title: "OsuSong.osk"},
-        {title: "osuSkin.osk"},
-        {title: "HelloWorld.py"}
-      ],
+      searchValue:"",
+      files: [],
     }
   }
 
@@ -25,6 +21,21 @@ class App extends Component {
   handleGenerate() {
     fetch('/generate').then(res => res.text()).then(key => this.setState({key}));
   }
+
+  handleValueChange(event) {
+    this.setState({ searchValue: event.target.value });
+  }
+
+  handleSearch() {
+    fetch(`/${this.state.searchValue}`).then(res => res.json()).then(files => this.setState({files}));
+  }
+
+  async handleDownload(keyPrefix,keyOriginal) {
+    fetch(`/file/${keyPrefix}/${keyOriginal}`).then(res => res.blob()).then(blob => download(blob,keyOriginal));
+    // let routeName = `/file/${keyPrefix}/${keyOriginal}`;
+    // console.log('CALLED DOWNLOAD?? TO THIS ROUTE--------->', routeName);
+  }
+
 
   render() {
 
@@ -36,7 +47,12 @@ class App extends Component {
           <Button type="default" onClick={this.handleGenerate.bind(this)} >Generate Key</Button>
         </div>
         <div className="inputs">
-          <Input.Search placeholder="Key" style={{ width:500 }}/>
+          <Input.Search
+            placeholder="Key"
+            style={{ width:500 }}
+            onChange={ this.handleValueChange.bind(this) }
+            onSearch={ this.handleSearch.bind(this) }
+          />
         </div>
         {
           this.state.key !== "" &&
@@ -56,7 +72,7 @@ class App extends Component {
         <List itemLayout="horizontal" dataSource={this.state.files} renderItem={item => (
             <List.Item>
               <List.Item.Meta
-                title={<a href="https://ant.design">{item.title}</a>}
+                title={<Button onClick={() => this.handleDownload(item.Key.substr(0,10),item.Key.substr(11,item.Key.length))} type='dashed'> {item.Key.substr(11,item.Key.length)} </Button> }
               />
           </List.Item>
           )}
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const AWS = require('aws-sdk');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
 const credentials = require('../credentials.js')
+const fs = require('fs');
 
 let key = "";
 
@@ -13,7 +14,7 @@ AWS.config.update({
   secretAccessKey: credentials.aws_s3_secret
 });
 
-let s3 = new AWS.S3({});
+let s3 = new AWS.S3({signatureVersion: 'v4', region: 'us-east-2'});
 
 let upload = multer({
   storage: multerS3({
@@ -33,17 +34,41 @@ router.get('/generate', (req,res) => {
   res.send(key);
 });
 
-router.get('/:fileKey', (req,res) => {
+router.get('/:id', (req,res) => {
   let params = {
     Bucket: "speedipigeon",
-
+    Prefix: req.params.id,
+    MaxKeys: 1000,
   };
-  s3.getObject()
-
+  s3.listObjects(params, (err, data) => {
+    let allFiles = [];
+    if(err) {
+      console.log(err, 'COULD NOT LIST');
+    }
+    else {
+      for(let index in data.Contents) {
+        allFiles.push(data.Contents[index]);
+      }
+    }
+    //console.log('THIS IS THE ARRAY', allFiles);
+    res.json(allFiles);
+  });
 });
 
 router.post('/upload',upload.any(), (req, res) => {
   res.send('Sucessfully uploaded');
+});
+
+router.get('/file/:fileName/:originalName', (req, res) => {
+  let keyfile = req.params.fileName + '/' + req.params.originalName;
+  console.log('KEY BEING PASSEDIN TO ROUTE======', keyfile);
+  let params = {
+    Bucket: "speedipigeon",
+    Key: keyfile,
+  }
+  res.attachment(keyfile);
+  s3.getObject(params).createReadStream().pipe(res);
+  console.log('SHOULD BE DOWNLAODED BY NOW');
 });
 
 
